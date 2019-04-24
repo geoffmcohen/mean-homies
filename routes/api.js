2527e81fe869692e351c130e83186e04b99a0de4
Geoff <geoffmcohen@gmail.com>
2019-04-24 17:12:14 -0600

    Adding rest end point for getting blog posts

--- routes/api.js
+++ routes/api.js
@@ -15,5 +15,57 @@ module.exports = (function(){
     res.send({data: 'test'});
   });
 
+  // Get blog posts for display
+  // #TODO: Need to figure out how errors should be handled here
+  api.get('/blog/get_posts', function(req, res){
+    var input = {postsPerPage: 3, page: 1};
+    if(req.query.page) input.page = req.query.page;
+
+    // Connect to the database
+    var MongoClient = require('mongodb').MongoClient;
+    var mongoURI = process.env.MONGOLAB_URI;
+    MongoClient.connect(mongoURI, {useNewUrlParser: true}, function(err, db){
+      // Callback with error if unable to connect
+      if(err){
+        console.log("Unable to connect to MongoDB!!!");
+        //return callback(err, null);
+        res.send({err: err, results: null});
+      }
+      // Get the collection
+      var dbo = db.db();
+      dbo.collection("blog", function(err, coll){
+        if(err){
+          console.log("Unable to get collection blog!!!");
+          //return callback(err, null);
+          res.send({err: err, results: null});
+        }
+        // Find all records sorted last to first
+        coll.find({}, {sort:{entryTime: -1}}, function(err, items){
+          if(err){
+            console.log("Unable to execute find on blog collection");
+            //return callback(err, null);
+            res.send({err: err, results: null});
+          }
+
+          // Get paged data
+          items = items.skip(input.postsPerPage * (input.page - 1)).limit(input.postsPerPage);
+
+          // Convert the cursor to an array
+          items.toArray(function(err, arr){
+            // Create return JSON with the array and information about paging
+            var ret = {blogPosts: arr, pageInfo: {currentPage: parseInt(input.page)}};
+
+            // Perform a count to determine how many pages total
+            coll.countDocuments(function(err, count){
+              ret.pageInfo.pageCount = Math.ceil(count / input.postsPerPage);
+              //return callback(err, ret);
+              res.send({err: err, results: ret});
+            });
+          });
+        });
+      });
+    });
+  });
+
   return api;
 })();
