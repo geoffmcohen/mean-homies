0117e8bb26f7a5dc5e803c9832cea316ffc31bcf
Geoff <geoffmcohen@gmail.com>
2019-05-30 08:59:00 -0600

    Removing environment file that should have been gitignore'd

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// Dynamically created by create_env.js
-export const environment = {
-  production: false,
-  google_maps_key: 'AIzaSyBjBGqRjrkV96GXvZoiXgP7HRdIzq7OEqA',
-  google_geocoder_key: 'AIzaSyARyQHIzZiki4DCMcJN1huck3sbWYRUjj8'
-};
