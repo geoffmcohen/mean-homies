fb81915c7d02eebf3d4e79be7fd0efd9f09f048e
Geoff <geoffmcohen@gmail.com>
2019-09-14 10:51:54 -0700

    Removing username at top of page and prod routing

--- src/app/app-routing.module.prod.ts
+++ src/app/app-routing.module.prod.ts
@@ -17,27 +17,35 @@ import { AboutComponent } from './home/about/about.component';
 import { FaqComponent } from './home/faq/faq.component';
 import { ContactComponent } from './home/contact/contact.component';
 import { EditProfileComponent } from './user/edit-profile/edit-profile.component';
+import { SearchComponent } from './search/search/search.component';
+import { HomiesComponent } from './homies/homies/homies.component';
+import { MessageCenterComponent } from './messages/message-center/message-center.component';
 import { TermsOfServiceComponent } from './terms/terms-of-service/terms-of-service.component';
 
 const routes: Routes = [
-  {path: '', component: ComingSoonComponent},
+  {path: '', component: ComingSoonComponent}, // Replace with commented out redirect
   {path: 'about', component: AboutComponent},
   {path: 'faq', component: FaqComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'terms-of-service', component: TermsOfServiceComponent},
   // {path: '', redirectTo: 'home', pathMatch: 'full'},
-  // {path: 'home', component: HomeComponent,
-  //   children: [
-  //     {path: '', component: DefaultComponent, pathMatch: 'full'},
-  //     {path: 'about', component: AboutComponent},
-  //     {path: 'faq', component: FaqComponent},
-  //     {path: 'user/edit-profile', component: EditProfileComponent}
-  //   ]
-  // },
+  {path: 'home', component: HomeComponent,
+    children: [
+      {path: '', component: DefaultComponent, pathMatch: 'full'},
+      {path: 'about', component: AboutComponent},
+      {path: 'faq', component: FaqComponent},
+      {path: 'contact', component: ContactComponent},
+      {path: 'user/edit-profile', component: EditProfileComponent},
+      {path: 'search', component: SearchComponent},
+      {path: 'homies', component: HomiesComponent},
+      {path: 'messages', component: MessageCenterComponent},
+      {path: 'terms-of-service', component: TermsOfServiceComponent}
+    ]
+  },
   {path: 'blog', component: BlogComponent},
   {path: 'blog/page/:pageNumber', component: BlogComponent},
   {path: 'admin', component: AdminComponent},
-  // {path: 'user/password-reset/:resetToken', component: PasswordResetComponent}
+  {path: 'user/password-reset/:resetToken', component: PasswordResetComponent}
 ];
 
 @NgModule({
